615d7032c59fc5b45484f4faeeb38d9c33d07ed4
BRENHINES <102440059+BRENHINES@users.noreply.github.com>
2023-12-01 03:01:06 -0500

    INIT: Initialisation of the clean architecture for the Events App Project

--- app/src/main/java/com/example/events/presentation/MainActivity.kt
+++ app/src/main/java/com/example/events/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.events
+package com.example.events.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.events.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
